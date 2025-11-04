5c4677170171195f01af6d8c59f93e8464b62786
3 sister company <Services@3sistersbranding.com>
2025-11-04 22:02:44 +0300

    Add files via upload

---
+++ FaqQuestionPage.js
@@ -0,0 +1,9 @@
+
+  document.getElementById("englishBtn").addEventListener("click", () => {
+    window.location.href = "FaqQueastionEnglis.html";  // Change to your English page URL
+  });
+
+  document.getElementById("arabicBtn").addEventListener("click", () => {
+    window.location.href = "QuestionCommon.html";  // Change to your Arabic page URL
+  });
+
---
+++ HomeSwitch.js
@@ -0,0 +1,9 @@
+
+  document.getElementById("englishBtn").addEventListener("click", () => {
+    window.location.href = "EnglishVersionHome.html";  // Change to your English page URL
+  });
+
+  document.getElementById("arabicBtn").addEventListener("click", () => {
+    window.location.href = "SecondVersionArabic2.html";  // Change to your Arabic page URL
+  });
+
