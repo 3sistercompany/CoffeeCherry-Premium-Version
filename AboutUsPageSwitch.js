591de035bddb9959b5dfabc42635e45278ef521d
3 sister company <Services@3sistersbranding.com>
2025-11-04 22:04:07 +0300

    Add files via upload

---
+++ AboutUsPageSwitch.js
@@ -0,0 +1,9 @@
+
+  document.getElementById("englishBtn").addEventListener("click", () => {
+    window.location.href = "AboutUsEnglishVersion.html";  // Change to your English page URL
+  });
+
+  document.getElementById("arabicBtn").addEventListener("click", () => {
+    window.location.href = "AboutUsArabicVersionSecond.html";  // Change to your Arabic page URL
+  });
+
