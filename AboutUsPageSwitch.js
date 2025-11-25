9137f196d26fb170822500e001c68255832346af
3 sister company <Services@3sistersbranding.com>
2025-11-25 22:18:44 +0300

    Delete AboutUsPageSwitch.js

--- AboutUsPageSwitch.js
+++
@@ -1,9 +0,0 @@
-
-  document.getElementById("englishBtn").addEventListener("click", () => {
-    window.location.href = "AboutUsEnglishVersion.html";  // Change to your English page URL
-  });
-
-  document.getElementById("arabicBtn").addEventListener("click", () => {
-    window.location.href = "AboutUsArabicVersionSecond.html";  // Change to your Arabic page URL
-  });
-
