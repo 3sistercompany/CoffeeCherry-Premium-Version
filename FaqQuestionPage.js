5c70baf71e268b3aea66fbaf9f3642307c166a11
3 sister company <Services@3sistersbranding.com>
2025-11-25 22:19:59 +0300

    Delete FaqQuestionPage.js

--- FaqQuestionPage.js
+++
@@ -1,9 +0,0 @@
-
-  document.getElementById("englishBtn").addEventListener("click", () => {
-    window.location.href = "FaqQueastionEnglis.html";  // Change to your English page URL
-  });
-
-  document.getElementById("arabicBtn").addEventListener("click", () => {
-    window.location.href = "QuestionCommon.html";  // Change to your Arabic page URL
-  });
-
